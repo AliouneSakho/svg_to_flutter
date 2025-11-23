924d3921aede2cf8ecbaff691f66c051e4dafead
Alioune Sakho <insakho46@gmail.com>
2025-11-23 18:36:00 +0000

    Update bin name

--- bin/svg_to_flutter.dart
+++ bin/svg_to_flutter.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:svg_to_flutter/svg_to_flutter.dart';
+import 'package:svg_to_flutter/main.dart';
 
 void main(List<String> arguments) async {
   exit(await SvgToFlutterCommandRunner().run(arguments));
